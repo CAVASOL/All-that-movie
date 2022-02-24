c3d024ea610595a054956c2e332adebf37202b3a
Yeon Kim <awyeon@gmail.com>
2022-02-24 18:01:10 +0800

    testing

--- app/javascript/packs/topnav.js
+++ app/javascript/packs/topnav.js
@@ -1,8 +1,10 @@
 function myFunction() {
-  var x = document.getElementById("myTopnav");
+  const x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += "responsive";
   } else {
     x.className = "topnav";
   }
 }
+
+export { myFunction }
